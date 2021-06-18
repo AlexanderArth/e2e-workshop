3dfb007d575d8f4a536c23708dac0bd6831b80b6
Joan Marc Pastor <extern.pastor-mackenzie_joan-marc@allianz.com>
2021-06-18 11:19:32 +0200

    login tested

--- cypress-project/cypress/exercises/exercise-1/login-page.spec.ts
+++ cypress-project/cypress/exercises/exercise-1/login-page.spec.ts
@@ -3,6 +3,7 @@ describe("Login page", () => {
     /*
      * Visit Login Page
      */
+    cy.visit('/login');
   });
 
   describe("Login page happy path", () => {
@@ -12,24 +13,33 @@ describe("Login page", () => {
        * when: user navigates to base-url/login
        * then: login form with inputs for user name and password and submit button is displayed
        */
+      cy.url().should('include', '/login');
+      cy.get('[data-cy="loginNameInput"]').should('be.visible');
+      cy.get('[data-cy="passwordInput"]').should('be.visible');
     });
-
+    
     it("should login successfully with correct credentials", () => {
       /*
-       * given: correct user credentials and user is not logged in
-       * when: user types correct username and password in the input fields on the login page and submits (or hits enter)
-       * then: redirect to landing page
-       */
+      * given: correct user credentials and user is not logged in
+      * when: user types correct username and password in the input fields on the login page and submits (or hits enter)
+      * then: redirect to landing page
+      */
+      cy.get('input[data-cy="loginNameInput"]').type('e2e-workshop@tngtech.com');
+      cy.get('input[data-cy="passwordInput"]').type('password');
+      cy.get('button[data-cy="loginButton"]').click();
+      cy.url().should('include', '/landing');
     });
   });
-
+  
   describe("Login form validation", () => {
     it("should disable login submission when no username is provided", () => {
       /*
-       * given: user is not logged in
-       * when: user types password
-       * then: submit button is disabled
-       */
+      * given: user is not logged in
+      * when: user types password
+      * then: submit button is disabled
+      */
+     cy.get('[data-cy="passwordInput"]').type('password');
+     cy.get('[data-cy="loginButton"]').should('be.disabled');
     });
 
     it("should disable login submission when no password is provided", () => {
@@ -38,6 +48,8 @@ describe("Login page", () => {
        * when: user types username (e2e-workshop@tngtech.com)
        * then: submit button is disabled
        */
+      cy.get('#mat-input-0').type('e2e-workshop@tngtech.com');
+      cy.findByRole('button', { name: /Login/i }).should('be.disabled');
     });
 
     it("should disable login submission when wrong username format is provided", () => {
@@ -46,6 +58,9 @@ describe("Login page", () => {
        * when: user types username e2e-workshop@tngtech and password
        * then: submit button is disabled
        */
+      cy.get('input[data-cy="loginNameInput"]').type('notanemail');
+      cy.get('input[data-cy="passwordInput"]').type('password');
+      cy.get('button[data-cy="loginButton"]').should('be.disabled');
     });
   });
 
@@ -56,9 +71,24 @@ describe("Login page", () => {
        * when: user types incorrect username and password in the input fields on the login page and submits (or hits enter)
        * then: dialog informing the user that wrong credentials were used is displayed
        */
+      cy.get('[data-cy="loginNameInput"]').type('e2e-workshop@tngtech.com');
+      cy.get('[data-cy="passwordInput"]').type('wrong-password');
+      cy.get('[data-cy="loginButton"]').click();
+      cy.get('[data-cy=errorDialog]').should('be.visible');
     });
+
   });
 
+});
+
+describe("Landing page", () => {
+  beforeEach(() => {
+    /*
+    * Visit Login Page
+    */
+   cy.visit('/landing');
+  });
+  
   describe("Route protection", () => {
     it("should redirect to login page from a valid subpage if user is not logged in", () => {
       /*
@@ -66,14 +96,29 @@ describe("Login page", () => {
        * when: user navigates to base-url/landing (or any other valid URL)
        * then: login form with inputs for user name and password and submit button is displayed
        */
+      cy.url().should('include', '/login');
     });
+  });
 
+});
+
+describe("Wrong page", () => {
+  beforeEach(() => {
+    /*
+    * Visit not valid page
+    */
+   cy.visit('/wrongpage');
+  });
+
+  describe("Route protection", () => {
     it("should redirect to login page from an invalid subpage if user is not logged in", () => {
       /*
        * given: user is not logged in
        * when: user navigates to base-url/xyz (or any other invalid URL)
        * then: login form with inputs for user name and password and submit button is displayed
        */
+      cy.url().should('include', '/login');
     });
   });
+
 });
--- cypress-project/cypress/exercises/exercise-2/landing-page.spec.ts
+++ cypress-project/cypress/exercises/exercise-2/landing-page.spec.ts
@@ -3,6 +3,11 @@ describe("Landing page", () => {
     /*
      * Visit Landing Page
      */
+      cy.visit('/login');
+      cy.get('input[data-cy="loginNameInput"]').type('e2e-workshop@tngtech.com');
+      cy.get('input[data-cy="passwordInput"]').type('password');
+      cy.get('button[data-cy="loginButton"]').click();
+      // cy.url().should('include', '/landing');
   });
 
   describe("Button click navigation", () => {
@@ -12,6 +17,9 @@ describe("Landing page", () => {
        * when: user clicks on logout button
        * then: user is redirected to login page
        */
+      cy.get('[data-cy=menuButton] > .mat-button-wrapper', {timeout: 5000}).click();
+      cy.get('[data-cy=logoutButton]').click();
+      cy.url().should('include', '/login');
     });
 
     it("should navigate to list of persons page", () => {
@@ -20,14 +28,18 @@ describe("Landing page", () => {
        * when: user clicks on button for list of persons
        * then: user is navigate to baseurl/list
        */
+      cy.get('[data-cy=personListButton]').click();
+      cy.url().should('include', '/list');
     });
 
-    it("should navigate to create person page", () => {
+    it.only("should navigate to create person page", () => {
       /*
        * given: user is logged in
        * when: user clicks on button for create person
        * then: user is navigate to baseurl/create
        */
+      cy.get('[data-cy=createPersonButton]').click();
+      cy.url().should('include', '/create');
     });
   });
 
